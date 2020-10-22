056875e373a6bafe377048039791d6f3bdde70ab
Jordy Gloudemans <jgloudemans@qblox.com>
2020-10-22 18:29:00 +0200

    Changed Pulsar backend version check from 0.1.1 to 0.1.2.

--- quantify/scheduler/backends/pulsar_backend.py
+++ quantify/scheduler/backends/pulsar_backend.py
@@ -19,8 +19,8 @@ from quantify.utilities.general import make_hash, without, import_func_from_stri
 PulsarModulations = namedtuple('PulsarModulations', ['gain', 'gain_Q', 'offset', 'phase', 'phase_delta'],
                                defaults=[None, None, None, None, None])
 
-QCM_DRIVER_VER = '0.1.1'
-QRM_DRIVER_VER = '0.1.1'
+QCM_DRIVER_VER = '0.1.2'
+QRM_DRIVER_VER = '0.1.2'
 
 
 class Q1ASMBuilder:
