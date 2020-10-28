f49b563aba1e2ead4e53cda96d0a157701d9de6e
CallumAttryde <callumattryde@gmail.com>
2020-10-28 16:52:09 +0100

    refactor visualisation out of backend module

--- quantify/scheduler/visualization/circuit_diagram.py
+++ quantify/scheduler/visualization/circuit_diagram.py
@@ -4,6 +4,8 @@
 # Copyright (C) Qblox BV & Orange Quantum Systems Holding BV (2020)
 # -----------------------------------------------------------------------------
 import quantify.scheduler.visualization.pulse_scheme as ps
+from quantify.scheduler.compilation import _determine_absolute_timing
+from quantify.utilities.general import import_func_from_string
 
 
 def gate_box(ax, time: float, qubit_idxs: list, tex: str, **kw):
@@ -43,3 +45,69 @@ def reset(ax, time: float, qubit_idxs: list, tex: str, **kw):
     """
     for qubit_idx in qubit_idxs:
         ps.box_text(ax, x0=time, y0=qubit_idx, text=tex, color='white', fillcolor='white', w=.4, h=.5, **kw)
+
+
+def circuit_diagram_matplotlib(schedule, figsize=None):
+    """
+    Creates a circuit diagram visualization of a schedule using matplotlib.
+
+    For this visualization backend to work, the schedule must contain `gate_info` for each operation in the
+    `operation_dict` as well as a value for `abs_time` for each element in the timing_constraints.
+
+    Parameters
+    ----------
+    schedule : :class:`~quantify.scheduler.types.Schedule`
+        the schedule to render.
+    figsize : tuple
+        matplotlib figsize.
+    Returns
+    -------
+    tuple
+        - matplotlib figure object.
+        - matplotlib axis object.
+    """
+    schedule = _determine_absolute_timing(schedule, 'ideal')
+
+    qubits = set()
+    for _, op in schedule.operations.items():
+        for qubit in op.data['gate_info']['qubits']:
+            qubits.add(qubit)
+    qubit_map = {}
+    for idx, qubit in enumerate(sorted(qubits)):
+        qubit_map[qubit] = idx
+
+    if figsize is None:
+        figsize = (10, len(qubit_map))
+    f, ax = ps.new_pulse_fig(figsize=figsize)
+    ax.set_title(schedule.data['name'])
+    ax.set_aspect('equal')
+
+    ax.set_ylim(-.5, len(qubit_map)-.5)
+    for q in qubits:
+        ax.axhline(qubit_map[q], color='.75')
+    # plot the qubit names on the y-axis
+    ax.set_yticks(list(qubit_map.values()))
+    ax.set_yticklabels(qubit_map.keys())
+
+    total_duration = 0
+    for t_constr in schedule.timing_constraints:
+        op = schedule.operations[t_constr['operation_hash']]
+        plot_func_name = op['gate_info']['plot_func']
+
+        # todo, hybrid visualisation
+        if plot_func_name is None:
+            op['gate_info']['plot_func'] = 'quantify.scheduler.visualization.circuit_diagram.gate_box'
+            op['gate_info']['tex'] = 'Pulse'
+            op['gate_info']['operation_type'] = 'Pulse'
+            for pulse in op['pulse_info']:
+                op['gate_info']['qubits'].append(pulse['channel'])
+
+        plot_func = import_func_from_string(op['gate_info']['plot_func'])
+        # A valid plot_func must accept the following arguments: ax, time (float), qubit_idxs (list), tex (str)
+        time = t_constr['abs_time']
+        idxs = [qubit_map[q] for q in op['gate_info']['qubits']]
+        plot_func(ax, time=time, qubit_idxs=idxs, tex=op['gate_info']['tex'])
+        total_duration = total_duration if total_duration > t_constr['abs_time'] else t_constr['abs_time']
+    ax.set_xlim(-1, total_duration + 1)
+
+    return f, ax
---
+++ tests/scheduler/visualization/test_circuit_diagram.py
@@ -0,0 +1,23 @@
+import pytest
+from quantify.scheduler import Schedule
+from quantify.scheduler.gate_library import Reset, Measure, CZ, CNOT, Rxy
+from quantify.scheduler.pulse_library import SquarePulse
+from quantify.scheduler.visualization.circuit_diagram import circuit_diagram_matplotlib
+
+
+@pytest.mark.mpl_image_compare(style='default', savefig_kwargs={'dpi': 300})
+def test_circuit_diagram_matplotlib():
+    sched = Schedule('Test experiment')
+
+    q0, q1 = ('q0', 'q1')
+    ref_label_1 = 'my_label'
+
+    sched.add(Reset(q0, q1))
+    sched.add(Rxy(90, 0, qubit=q0), label=ref_label_1)
+    sched.add(SquarePulse(0.8, 20e-9, ch='q0'))  # will change with API update
+    sched.add(operation=CNOT(qC=q0, qT=q1))
+    sched.add(Rxy(theta=90, phi=0, qubit=q0))
+    sched.add(Measure(q0, q1), label='M0')
+
+    f, ax = circuit_diagram_matplotlib(sched)
+    return f
