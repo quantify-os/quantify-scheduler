3901d26516b96ba6ebe4812cd269872af9f5aa73
CallumAttryde <callumattryde@gmail.com>
2020-12-10 17:08:23 +0100

    hotfix

--- quantify/scheduler/backends/pulsar_backend.py
+++ quantify/scheduler/backends/pulsar_backend.py
@@ -319,16 +319,15 @@ def _extract_nco_freq(hardware_mapping: dict, hw_mapping_inverted: dict, port: s
         raise ValueError("frequency under constrained, specify either the lo_freq or nco_freq in the hardware mapping")
     elif lo_freq is None and nco_freq is not None:
         # LO = RF - IF
-        lo_freq = clock_freq - nco_freq
+        return clock_freq - nco_freq
     elif nco_freq is None and lo_freq is not None:
         # RF - LO = IF
-        nco_freq = clock_freq - lo_freq
+        return clock_freq - lo_freq
     elif lo_freq is not None and nco_freq is not None:
         raise ValueError("frequency over constrained, do not specify both "
                          "the lo_freq and nco_freq in the hardware mapping.")
-
-    # FIXME, maybe we always want to return all three.
-    return nco_freq
+    else:
+        raise ValueError("clock frequency undefined.")
 
 
 def _extract_io(hardware_mapping: dict, hw_mapping_inverted: dict, port: str, clock: str):
