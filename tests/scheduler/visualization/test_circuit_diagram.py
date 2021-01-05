07d8d369fea89d5f9233d73ca2fb21d9ba3ea0e9
Thomas Reynders <thomas@orangeqs.com>
2021-01-05 11:58:35 +0100

    style(test_circuit_diagram): Clean up code
    - Add baseline_dir to mpl_image_compare mark
    - Rename 'sched' to 'schedule'. Using abbreviations is bad practice.
    - Ignore unused variable 'ax'

--- tests/scheduler/visualization/test_circuit_diagram.py
+++ tests/scheduler/visualization/test_circuit_diagram.py
@@ -6,35 +6,35 @@ from quantify.scheduler.visualization.circuit_diagram import circuit_diagram_mat
 import matplotlib.pyplot as plt
 
 
-@pytest.mark.mpl_image_compare(style='default', savefig_kwargs={'dpi': 300})
+@pytest.mark.mpl_image_compare(baseline_dir='baseline_images', style='default', savefig_kwargs={'dpi': 300})
 def test_circuit_diagram_matplotlib():
-    sched = Schedule('Test experiment')
+    schedule = Schedule('Test experiment')
 
     q0, q1 = ('q0', 'q1')
     ref_label_1 = 'my_label'
 
-    sched.add(Reset(q0, q1))
-    sched.add(Rxy(90, 0, qubit=q0), label=ref_label_1)
-    sched.add(operation=CNOT(qC=q0, qT=q1))
-    sched.add(Rxy(theta=90, phi=0, qubit=q0))
-    sched.add(Measure(q0, q1), label='M0')
+    schedule.add(Reset(q0, q1))
+    schedule.add(Rxy(90, 0, qubit=q0), label=ref_label_1)
+    schedule.add(operation=CNOT(qC=q0, qT=q1))
+    schedule.add(Rxy(theta=90, phi=0, qubit=q0))
+    schedule.add(Measure(q0, q1), label='M0')
 
-    f, ax = circuit_diagram_matplotlib(sched)
+    f, _ = circuit_diagram_matplotlib(schedule)
     return f
 
 
 @pytest.mark.mpl_image_compare(style='default', savefig_kwargs={'dpi': 300})
 def test_hybrid_circuit_diagram_matplotlib():
-    sched = Schedule('Test experiment')
+    schedule = Schedule('Test experiment')
 
     q0, q1 = ('q0', 'q1')
     ref_label_1 = 'my_label'
 
-    sched.add(Reset(q0, q1))
-    sched.add(Rxy(90, 0, qubit=q0), label=ref_label_1)
-    sched.add(SquarePulse(0.8, 20e-9, port='q0:mw', clock='cl0.baseband'))
-    sched.add(operation=CNOT(qC=q0, qT=q1))
-    sched.add(Measure(q0, q1), label='M0')
+    schedule.add(Reset(q0, q1))
+    schedule.add(Rxy(90, 0, qubit=q0), label=ref_label_1)
+    schedule.add(SquarePulse(0.8, 20e-9, port='q0:mw', clock='cl0.baseband'))
+    schedule.add(operation=CNOT(qC=q0, qT=q1))
+    schedule.add(Measure(q0, q1), label='M0')
 
-    f, ax = circuit_diagram_matplotlib(sched)
+    f, _ = circuit_diagram_matplotlib(schedule)
     return f
