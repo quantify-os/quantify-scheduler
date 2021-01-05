327d5429814c2028cec4eef362b04bbf74673a41
Thomas Reynders <thomas@orangeqs.com>
2021-01-05 11:52:05 +0100

    feat(compilation): Add typing 'Schedule' to function parameters

--- quantify/scheduler/compilation.py
+++ quantify/scheduler/compilation.py
@@ -3,6 +3,7 @@
 # Repository:     https://gitlab.com/quantify-os/quantify-scheduler
 # Copyright (C) Qblox BV & Orange Quantum Systems Holding BV (2020-2021)
 # -----------------------------------------------------------------------------
+from typing import TYPE_CHECKING, Callable
 import logging
 import jsonschema
 import importlib
@@ -11,8 +12,11 @@ from quantify.scheduler.resources import ClockResource, BasebandClockResource
 from quantify.scheduler.pulse_library import SquarePulse, DRAGPulse, IdlePulse, SoftSquarePulse
 from quantify.utilities.general import load_json_schema
 
+if TYPE_CHECKING:
+    from quantify.scheduler import Schedule
 
-def determine_absolute_timing(schedule, time_unit='physical'):
+
+def determine_absolute_timing(schedule: 'Schedule', time_unit='physical'):
     """
     Determines the absolute timing of a schedule based on the timing constraints.
 
@@ -97,7 +101,7 @@ def _find_edge(device_cfg, q0, q1, op_name):
     return edge_cfg
 
 
-def add_pulse_information_transmon(schedule, device_cfg: dict):
+def add_pulse_information_transmon(schedule: 'Schedule', device_cfg: dict):
     """
     Adds pulse information specified in the device config to the schedule.
 
@@ -250,7 +254,7 @@ def validate_config(config: dict, scheme_fn: str):
     return True
 
 
-def qcompile(schedule: Schedule, device_cfg: dict,
+def qcompile(schedule: 'Schedule', device_cfg: dict,
              hardware_mapping: dict = None, **kwargs):
     """
     Compile and assemble a schedule into deployables.
