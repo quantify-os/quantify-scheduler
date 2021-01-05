a8dcd3bf57b6022bacca74408f8b452408590e17
Thomas Reynders <thomas@orangeqs.com>
2021-01-05 15:19:25 +0100

    feat(circuit_diagram): Add drawing 'baseband' and 'modulated' pulse to circuit_diagram_matplotlib
    - The baseband pulse is drawn when the frequence is set to 0 else an modulated pulse is drawn.
    - Added circuit_diagram.pulse_baseband function
    - Added circuit_diagram.pulse_modulated function
    - Adopt PEP563 for better type hinting

--- quantify/scheduler/visualization/circuit_diagram.py
+++ quantify/scheduler/visualization/circuit_diagram.py
@@ -3,6 +3,7 @@
 # Repository:     https://gitlab.com/qblox/packages/software/quantify/
 # Copyright (C) Qblox BV & Orange Quantum Systems Holding BV (2020-2021)
 # -----------------------------------------------------------------------------
+from __future__ import annotations
 from typing import TYPE_CHECKING, Tuple, Union, List
 import logging
 import quantify.scheduler.visualization.pulse_scheme as ps
@@ -16,7 +17,7 @@ if TYPE_CHECKING:
     from matplotlib.axes import Axes
 
 
-def gate_box(ax, time: float, qubit_idxs: List[int], tex: str, **kw):
+def gate_box(ax: Axes, time: float, qubit_idxs: List[int], tex: str, **kw):
     """
     A box for a single gate containing a label.
     """
@@ -24,13 +25,25 @@ def gate_box(ax, time: float, qubit_idxs: List[int], tex: str, **kw):
         ps.box_text(ax, x0=time, y0=qubit_idx, text=tex, fillcolor='C0', w=.8, h=.5, **kw)
 
 
-def pulse_box(ax, time: float, qubit_idxs: List[int], tex: str, **kw):
+def pulse_baseband(ax: Axes, time: float, qubit_idxs: List[int], tex: str, **kw):
+    """
+    Adds a visual indicator for a Baseband pulse to the `mathplotlib.axes.Axis` instance.
+    """
     for qubit_idx in qubit_idxs:
-        ps.box_text(ax, x0=time, y0=qubit_idx, fillcolor='w', w=.66, h=.3, **kw)
-        ax.text(time, qubit_idx+0.3, tex, ha='center', va='center', zorder=6)
+        ps.fluxPulse(ax, pos=time, y_offs=qubit_idx, width=.4, s=0.0025, amp=.33, **kw)
+        ax.text(time+.2, qubit_idx+0.45, tex, ha='center', va='center', zorder=6)
 
 
-def meter(ax, time: float, qubit_idxs: List[int], tex: str, **kw):
+def pulse_modulated(ax: Axes, time: float, qubit_idxs: List[int], tex: str, **kw):
+    """
+    Adds a visual indicator for a Modulated pulse to the `mathplotlib.axes.Axis` instance.
+    """
+    for qubit_idx in qubit_idxs:
+        ps.mwPulse(ax, pos=time, y_offs=qubit_idx, width=.4, amp=.33, **kw)
+        ax.text(time+.2, qubit_idx+0.45, tex, ha='center', va='center', zorder=6)
+
+
+def meter(ax: Axes, time: float, qubit_idxs: List[int], tex: str, **kw):
     """
     A simple meter to depict a measurement.
     """
@@ -38,7 +51,7 @@ def meter(ax, time: float, qubit_idxs: List[int], tex: str, **kw):
         ps.meter(ax, x0=time, y0=qubit_idx, fillcolor='C4', y_offs=0, w=.8, h=.5, **kw)
 
 
-def cnot(ax, time: float, qubit_idxs: List[int], tex: str, **kw):
+def cnot(ax: Axes, time: float, qubit_idxs: List[int], tex: str, **kw):
     """
     Markers to denote a CNOT gate between two qubits.
     """
@@ -46,14 +59,14 @@ def cnot(ax, time: float, qubit_idxs: List[int], tex: str, **kw):
     ax.plot([time], qubit_idxs[1], marker='+', markersize=12, color='white')
 
 
-def cz(ax, time: float, qubit_idxs: List[int], tex: str, **kw):
+def cz(ax: Axes, time: float, qubit_idxs: List[int], tex: str, **kw):
     """
     Markers to denote a CZ gate between two qubits.
     """
     ax.plot([time, time], qubit_idxs, marker='o', markersize=15, color='C1')
 
 
-def reset(ax, time: float, qubit_idxs: List[int], tex: str, **kw):
+def reset(ax: Axes, time: float, qubit_idxs: List[int], tex: str, **kw):
     """
     A broken line to denote qubit initialization.
     """
@@ -71,7 +84,7 @@ def _locate_qubit_in_address(qubit_map, address):
     raise ValueError("Could not resolve address '{}'".format(address))
 
 
-def circuit_diagram_matplotlib(schedule: 'Schedule', figsize=None) -> Tuple['Figure', Union['Axes', List['Axes']]]:
+def circuit_diagram_matplotlib(schedule: Schedule, figsize=None) -> Tuple[Figure, Union[Axes, List[Axes]]]:
     """
     Creates a circuit diagram visualization of a schedule using matplotlib.
 
@@ -116,6 +129,7 @@ def circuit_diagram_matplotlib(schedule: 'Schedule', figsize=None) -> Tuple['Fig
     total_duration = 0
     for t_constr in schedule.timing_constraints:
         op = schedule.operations[t_constr['operation_hash']]
+
         time = t_constr['abs_time']
         total_duration = total_duration if total_duration > time else time
 
@@ -124,9 +138,15 @@ def circuit_diagram_matplotlib(schedule: 'Schedule', figsize=None) -> Tuple['Fig
             idxs = [qubit_map[q] for q in op['gate_info']['qubits']]
             plot_func(ax, time=time, qubit_idxs=idxs, tex=op['gate_info']['tex'])
         elif op.valid_pulse:
-            logging.warning("Elided drawing of pulse in circuit diagram at time '{}' (not currently supported)."
-                            .format(time))
-            continue
+            idxs = [qubit_map[_locate_qubit_in_address(qubit_map, pulse_info['port'])]
+                    for pulse_info in op['pulse_info']]
+            for pulse_info in op["pulse_info"]:
+                clock_id: str = pulse_info['clock']
+                clock_resource: dict = schedule.data["resource_dict"][clock_id]
+                if clock_resource['freq'] == 0:
+                    pulse_baseband(ax, time=time, qubit_idxs=idxs, tex=op.name)
+                else:
+                    pulse_modulated(ax, time=time, qubit_idxs=idxs, tex=op.name)
         else:
             raise ValueError("Unknown operation")
 
--- tests/scheduler/visualization/test_circuit_diagram.py
+++ tests/scheduler/visualization/test_circuit_diagram.py
@@ -2,12 +2,13 @@ import pytest
 from quantify.scheduler import Schedule
 from quantify.scheduler.gate_library import Reset, Measure, CNOT, Rxy
 from quantify.scheduler.pulse_library import SquarePulse
+from quantify.scheduler.resources import ClockResource
 from quantify.scheduler.visualization.circuit_diagram import circuit_diagram_matplotlib
 import matplotlib.pyplot as plt
 
 
 @pytest.mark.mpl_image_compare(baseline_dir='baseline_images', style='default', savefig_kwargs={'dpi': 300})
-def test_circuit_diagram_matplotlib():
+def test_hybrid_circuit_diagram_baseband_matplotlib():
     schedule = Schedule('Test experiment')
 
     q0, q1 = ('q0', 'q1')
@@ -23,7 +24,7 @@ def test_circuit_diagram_matplotlib():
     return f
 
 
-@pytest.mark.mpl_image_compare(style='default', savefig_kwargs={'dpi': 300})
+@pytest.mark.mpl_image_compare(baseline_dir='baseline_images', style='default', savefig_kwargs={'dpi': 300})
 def test_hybrid_circuit_diagram_matplotlib():
     schedule = Schedule('Test experiment')
 
@@ -33,6 +34,25 @@ def test_hybrid_circuit_diagram_matplotlib():
     schedule.add(Reset(q0, q1))
     schedule.add(Rxy(90, 0, qubit=q0), label=ref_label_1)
     schedule.add(SquarePulse(0.8, 20e-9, port='q0:mw', clock='cl0.baseband'))
+    schedule.add(operation=CNOT(qC=q0, qT=q1))
+    schedule.add(Measure(q0, q1), label='M0')
+
+    f, _ = circuit_diagram_matplotlib(schedule)
+    return f
+
+
+@pytest.mark.mpl_image_compare(baseline_dir='baseline_images', style='default', savefig_kwargs={'dpi': 300})
+def test_hybrid_circuit_diagram_modulated_matplotlib():
+    schedule = Schedule('Test experiment')
+
+    q0, q1 = ('q0', 'q1')
+    ref_label_1 = 'my_label'
+
+    schedule.add_resource(ClockResource(name="q0.01", freq=6.02e9))
+
+    schedule.add(Reset(q0, q1))
+    schedule.add(Rxy(90, 0, qubit=q0), label=ref_label_1)
+    schedule.add(SquarePulse(0.8, 20e-9, port='q0:mw', clock='q0.01'))
     schedule.add(operation=CNOT(qC=q0, qT=q1))
     schedule.add(Measure(q0, q1), label='M0')
 
