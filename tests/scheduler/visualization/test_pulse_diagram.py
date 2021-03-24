2d646a1c4bf2714ccb8cbc37b59e90ffc3946371
caenrigen <vnegirneac@qblox.com>
2021-03-24 20:53:29 +0100

    move tests

---
+++ tests/scheduler/visualization/test_pulse_diagram.py
@@ -0,0 +1,32 @@
+import json
+import inspect
+from pathlib import Path
+
+from quantify.scheduler import Schedule
+from quantify.scheduler.gate_library import Reset, Measure, Rxy
+from quantify.scheduler.compilation import qcompile
+import quantify.scheduler.visualization.pulse_diagram as plsd
+import quantify.scheduler.schemas.examples as es
+
+
+esp = inspect.getfile(es)
+cfg_f = Path(esp).parent / "transmon_test_config.json"
+with open(cfg_f, "r") as f:
+    DEVICE_CFG = json.load(f)
+
+# todo, verification of this, probably requires some horrible selenium malarkey
+def test_pulse_diagram_plotly():
+    sched = Schedule("Test schedule")
+
+    # define the resources
+    q0, q1 = ("q0", "q1")
+    sched.add(Reset(q0, q1))
+    sched.add(Rxy(90, 0, qubit=q0))
+    # sched.add(operation=CZ(qC=q0, qT=q1)) # not implemented in config
+    sched.add(Rxy(theta=90, phi=0, qubit=q0))
+    sched.add(Measure(q0, q1), label="M0")
+    # pulse information is added
+    sched = qcompile(sched, DEVICE_CFG, None)
+
+    # It should be possible to generate this visualization after compilation
+    _ = plsd.pulse_diagram_plotly(sched)
--- tests/scheduler/visualization/test_pulse_scheme.py
+++ tests/scheduler/visualization/test_pulse_scheme.py
@@ -1,19 +1,6 @@
 import matplotlib.pyplot as plt
 import quantify.scheduler.visualization.pulse_scheme as pls
 import pytest
-from quantify.scheduler import Schedule
-from quantify.scheduler.gate_library import Reset, Measure, Rxy
-from quantify.scheduler.compilation import qcompile
-import inspect
-import json
-import os
-import quantify.scheduler.schemas.examples as es
-
-esp = inspect.getfile(es)
-cfg_f = os.path.abspath(os.path.join(esp, "..", "transmon_test_config.json"))
-with open(cfg_f, "r") as f:
-    DEVICE_CFG = json.load(f)
-
 
 cm = 1 / 2.54  # inch to cm conversion
 
@@ -61,23 +48,3 @@ def test_plot_pulses_n_q():
 
     fig.subplots_adjust(left=0.07, top=0.9, hspace=0.1)
     return fig
-
-
-# todo, verification of this, probably requires some horrible selenium malarkey
-def test_pulse_diagram_plotly():
-    sched = Schedule("Test schedule")
-
-    # define the resources
-    q0, q1 = ("q0", "q1")
-    sched.add(Reset(q0, q1))
-    sched.add(Rxy(90, 0, qubit=q0))
-    # sched.add(operation=CZ(qC=q0, qT=q1)) # not implemented in config
-    sched.add(Rxy(theta=90, phi=0, qubit=q0))
-    sched.add(Measure(q0, q1), label="M0")
-    # pulse information is added
-    sched = qcompile(sched, DEVICE_CFG, None)
-
-    # It should be possible to generate this visualization after compilation
-    _ = pls.pulse_diagram_plotly(sched)
-    # and with auto labels
-    _ = pls.pulse_diagram_plotly(sched)
