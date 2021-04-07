475ba3c1f8bf8657fc7fa2a91829b69dad07a3c4
caenrigen <vnegirneac@qblox.com>
2021-04-07 19:16:48 +0200

    fixed more linting

--- tests/scheduler/visualization/test_pulse_diagram.py
+++ tests/scheduler/visualization/test_pulse_diagram.py
@@ -1,3 +1,7 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+
 import json
 import inspect
 from pathlib import Path
@@ -14,17 +18,17 @@ cfg_f = Path(esp).parent / "transmon_test_config.json"
 with open(cfg_f, "r") as f:
     DEVICE_CFG = json.load(f)
 
-# todo, verification of this, probably requires some horrible selenium malarkey
+# Proper verification of this, probably requires some horrible selenium malarkey
 def test_pulse_diagram_plotly():
     sched = Schedule("Test schedule")
 
     # define the resources
-    q0, q1 = ("q0", "q1")
-    sched.add(Reset(q0, q1))
-    sched.add(Rxy(90, 0, qubit=q0))
-    # sched.add(operation=CZ(qC=q0, qT=q1)) # not implemented in config
-    sched.add(Rxy(theta=90, phi=0, qubit=q0))
-    sched.add(Measure(q0, q1), label="M0")
+    qubit_0, qubit_1 = ("qubit_0", "qubit_1")
+    sched.add(Reset(qubit_0, qubit_1))
+    sched.add(Rxy(90, 0, qubit=qubit_0))
+    # sched.add(operation=CZ(qC=qubit_0, qT=qubit_1)) # not implemented in config
+    sched.add(Rxy(theta=90, phi=0, qubit=qubit_0))
+    sched.add(Measure(qubit_0, qubit_1), label="M0")
     # pulse information is added
     sched = qcompile(sched, DEVICE_CFG, None)
 
