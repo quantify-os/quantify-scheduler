be7c68df349eee90a2399744f128e89f94e2a651
caenrigen <vnegirneac@qblox.com>
2021-04-08 12:44:09 +0200

    prep headers for future merge

--- quantify/scheduler/visualization/pulse_diagram.py
+++ quantify/scheduler/visualization/pulse_diagram.py
@@ -1,8 +1,6 @@
-# -----------------------------------------------------------------------------
-# Description:    Functions for drawing pulse diagrams
-# Repository:     https://gitlab.com/quantify-os/quantify-scheduler
-# Copyright (C) Qblox BV & Orange Quantum Systems Holding BV (2020-2021)
-# -----------------------------------------------------------------------------
+# Repository: https://gitlab.com/quantify-os/quantify-scheduler
+# Licensed according to the LICENCE file on the master branch
+"""Functions for drawing pulse diagrams"""
 from __future__ import annotations
 import inspect
 import logging
