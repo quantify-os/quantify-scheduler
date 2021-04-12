3481d20badf62ca64b2d18ee1b4a9ccc35fd1147
Damien <damiencrielaard@gmail.com>
2021-04-12 15:08:32 +0200

    added copyright header

--- quantify/scheduler/backends/types/qblox.py
+++ quantify/scheduler/backends/types/qblox.py
@@ -1,3 +1,7 @@
+# Repository: https://gitlab.com/quantify-os/quantify-scheduler
+# Licensed according to the LICENCE file on the master branch
+"""Python dataclasses for compilation to Qblox hardware."""
+
 import numpy as np
 
 from typing import Optional
