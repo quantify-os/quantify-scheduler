6ad1ec9c9701ef28e04b125b3906047e77657d87
Damien <damiencrielaard@gmail.com>
2021-04-14 14:01:35 +0200

    fixed incorrect amplitude

--- quantify/scheduler/backends/qblox_backend.py
+++ quantify/scheduler/backends/qblox_backend.py
@@ -896,7 +896,7 @@ class QASMProgram(list):
                 f"{param} is set to {val}. Parameter must be in the range "
                 f"-1.0 <= param <= 1.0 for {repr(operation)}."
             )
-        return int(val * immediate_sz / 2)
+        return int(val * immediate_sz // 2)
 
     @staticmethod
     def to_pulsar_time(time: float) -> int:
@@ -1292,14 +1292,10 @@ class Pulsar_sequencer_base(metaclass=ABCMeta):
         """
         amp_real, amp_imag = np.max(np.abs(data.real)), np.max(np.abs(data.imag))
         norm_data_r = (
-            data.real / amp_real / self.AWG_OUTPUT_VOLT
-            if amp_real != 0.0
-            else np.zeros(data.real.shape)
+            data.real / amp_real if amp_real != 0.0 else np.zeros(data.real.shape)
         )
         norm_data_i = (
-            data.imag / amp_imag / self.AWG_OUTPUT_VOLT
-            if amp_imag != 0.0
-            else np.zeros(data.imag.shape)
+            data.imag / amp_imag if amp_imag != 0.0 else np.zeros(data.imag.shape)
         )
         return norm_data_r + 1.0j * norm_data_i, amp_real, amp_imag
 
