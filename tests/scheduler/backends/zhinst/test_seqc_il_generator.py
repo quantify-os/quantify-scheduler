b2e5f84037760fa02e2d1677da755119c3992235
Thomas Reynders <thomas.reynders@topic.nl>
2021-05-14 19:42:46 +0000

    feat(zhinst): Add ZISettingsBuilder class

--- tests/scheduler/backends/zhinst/test_seqc_il_generator.py
+++ tests/scheduler/backends/zhinst/test_seqc_il_generator.py
@@ -1,8 +1,6 @@
-# -----------------------------------------------------------------------------
-# Description:    Tests for Zurich Instruments backend.
-# Repository:     https://gitlab.com/quantify-os/quantify-scheduler
-# Copyright (C) Qblox BV & Orange Quantum Systems Holding BV (2020-2021)
-# -----------------------------------------------------------------------------
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
 from textwrap import dedent
 
 import pytest
