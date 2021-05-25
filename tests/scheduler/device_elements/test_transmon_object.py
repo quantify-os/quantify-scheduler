8fd2af69291cb08e87917d7c3156ff3dab9a9b2a
adamorangeqs <adam@orangeqs.com>
2021-05-25 17:20:51 +0200

    re-write tests

--- tests/scheduler/device_elements/test_transmon_object.py
+++ tests/scheduler/device_elements/test_transmon_object.py
@@ -1,41 +1,39 @@
 # pylint: disable=missing-module-docstring
 # pylint: disable=missing-class-docstring
 # pylint: disable=missing-function-docstring
-
+import pytest
 from quantify.scheduler.compilation import validate_config
 from quantify.scheduler.device_elements.transmon_element import TransmonElement
 
 
-class TestTransmonElement:
-    @classmethod
-    def setup_class(cls):
-        cls.q0 = TransmonElement("q0")
+@pytest.fixture
+def q_0() -> TransmonElement:
+    q_0 = TransmonElement("q0")
 
-    @classmethod
-    def teardown_class(cls):
-        for inststr in list(cls.q0._all_instruments):
-            try:
-                inst = cls.q0.find_instrument(inststr)
-                inst.close()
-            except KeyError:
-                pass
+    # Transmon element is returned
+    yield q_0
+    # after the test, teardown...
+    q_0.close()
 
-    def test_qubit_name(self):
-        assert self.q0.name == "q0"
 
-    def test_generate_qubit_config(self):
+def test_qubit_name(q_0: TransmonElement):
+    assert q_0.name == "q0"
 
-        # set some values
-        self.q0.ro_pulse_type("square")
-        self.q0.ro_pulse_duration(400e-9)
 
-        q_cfg = self.q0.generate_qubit_config()
+def test_generate_qubit_config(q_0: TransmonElement):
 
-        # assert values in right place in config.
-        assert q_cfg["q0"]["resources"]["port_mw"] == "q0:mw"
-        assert q_cfg["q0"]["params"]["ro_pulse_type"] == "square"
-        assert q_cfg["q0"]["params"]["ro_pulse_duration"] == 400e-9
+    # set some values
+    q_0.ro_pulse_type("square")
+    q_0.ro_pulse_duration(400e-9)
 
-    def test_generate_device_config(self):
-        dev_cfg = self.q0.generate_device_config()
-        assert validate_config(dev_cfg, scheme_fn="transmon_cfg.json")
+    q_cfg = q_0.generate_qubit_config()
+
+    # assert values in right place in config.
+    assert q_cfg["q0"]["resources"]["port_mw"] == "q0:mw"
+    assert q_cfg["q0"]["params"]["ro_pulse_type"] == "square"
+    assert q_cfg["q0"]["params"]["ro_pulse_duration"] == 400e-9
+
+
+def test_generate_device_config(q_0: TransmonElement):
+    dev_cfg = q_0.generate_device_config()
+    assert validate_config(dev_cfg, scheme_fn="transmon_cfg.json")
