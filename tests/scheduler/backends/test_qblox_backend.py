4eac31ece37e2ef310827a85cdf328b70dde1c42
Damien <damiencrielaard@gmail.com>
2021-06-09 16:49:05 +0200

    added add from str test

--- tests/scheduler/backends/test_qblox_backend.py
+++ tests/scheduler/backends/test_qblox_backend.py
@@ -679,5 +679,17 @@ def test_container_add_instrument_compiler(pulse_only_schedule):
 
 def test_container__add_from_type(pulse_only_schedule):
     container = compiler_container.CompilerContainer(pulse_only_schedule)
+    container._add_from_type("qcm0", Pulsar_QCM, HARDWARE_MAPPING["qcm0"])
+    assert "qcm0" in container.instrument_compilers
+
+
+def test_container__add_from_type_indirect(pulse_only_schedule):
+    container = compiler_container.CompilerContainer(pulse_only_schedule)
     container.add_instrument_compiler("qcm0", Pulsar_QCM, HARDWARE_MAPPING["qcm0"])
+    assert "qcm0" in container.instrument_compilers
+
+
+def test_container__add_from_str(pulse_only_schedule):
+    container = compiler_container.CompilerContainer(pulse_only_schedule)
+    container._add_from_str("qcm0", "Pulsar_QCM", HARDWARE_MAPPING["qcm0"])
     assert "qcm0" in container.instrument_compilers
