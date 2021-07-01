17382e1b7c88a025b5642e7480faac26d500c6af
adamorangeqs <adam@orangeqs.com>
2021-07-01 11:39:46 +0200

    add control stack test

--- tests/scheduler/device_elements/test_transmon_element.py
+++ tests/scheduler/device_elements/test_transmon_element.py
@@ -3,18 +3,21 @@
 # pylint: disable=missing-function-docstring
 # pylint: disable=redefined-outer-name
 import pytest
+from quantify_scheduler.controlstack.station import ControlStack
 from quantify_scheduler.compilation import validate_config
 from quantify_scheduler.device_elements.transmon_element import TransmonElement
 
 
 @pytest.fixture
 def q_0() -> TransmonElement:
-    q_0 = TransmonElement("q0")
+    control_stack = ControlStack("cs")
+    q_0 = TransmonElement("q0", control_stack="cs")
 
     # Transmon element is returned
     yield q_0
     # after the test, teardown...
     q_0.close()
+    control_stack.close()
 
 
 def test_qubit_name(q_0: TransmonElement):
@@ -38,3 +41,8 @@ def test_generate_config(q_0: TransmonElement):
 def test_generate_device_config(q_0: TransmonElement):
     dev_cfg = q_0.generate_device_config()
     assert validate_config(dev_cfg, scheme_fn="transmon_cfg.json")
+
+
+def test_find_control_stack(q_0: TransmonElement):
+    control_stack = q_0.control_stack.get_instr()
+    assert control_stack.name == "cs"
