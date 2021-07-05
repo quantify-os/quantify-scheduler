9fb24d52961f1fbb2defcfd48eb90db5f41e3c65
Damien <damiencrielaard@gmail.com>
2021-07-05 10:09:18 +0200

    docstring interpolated_waveform

--- quantify_scheduler/waveforms.py
+++ quantify_scheduler/waveforms.py
@@ -231,6 +231,27 @@ def drag(
 def interpolated_waveform(
     t: np.ndarray, samples: np.ndarray, t_samples: np.ndarray, **kwargs
 ):
+    """
+    Wrapper function around `scipy.interpolate.interp1d`, which takes the array of
+    (complex) samples, interpolates the real and imaginary parts separately and returns
+    the interpolated values at the specified times.
+
+    Parameters
+    ----------
+    t
+        Times at which to evaluated the to be returned waveform.
+    samples
+        An array of (possibly complex) values specifying the shape of the waveform.
+    t_samples
+        An array of values specifying the corresponding times at which the `samples`
+        are evaluated.
+    kwargs
+        Optional keyword arguments to pass to `scipy.interpolate.interp1d`.
+
+    Returns
+    -------
+        An array containing the interpolated values.
+    """
     real_interpolator = interpolate.interp1d(t_samples, samples.real, **kwargs)
     imag_interpolator = interpolate.interp1d(t_samples, samples.imag, **kwargs)
     return real_interpolator(t) + 1.0j * imag_interpolator(t)
