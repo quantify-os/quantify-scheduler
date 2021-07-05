ee2310ff925ae4569be4f6e7552e5a57a01572ac
kel85uk <kel85uk@gmail.com>
2021-07-05 14:28:01 +0200

    Fixes the setup.py file to track develop of quantify-core instead.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ requirements = [
     "zhinst-qcodes",
     "pathvalidate",
     "typing_extensions",
-    "quantify_core@git+https://gitlab.com/quantify-os/quantify-core/#629f212927ab1c001b0ced1aba896ffc7f77bcc0",
+    "quantify-core@git+https://gitlab.com/quantify-os/quantify-core@develop#egg=quantify-core",
     "qblox-instruments==0.3.2",
 ]
 
