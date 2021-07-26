5742f07dd9cd703f5b89a482336f648176ec778c
Diogo Valada <dvalada@qblox.com>
2021-07-26 15:57:06 +0200

    Fixed PulsarRFSettings retrieval of lo1_freq

--- quantify_scheduler/backends/types/qblox.py
+++ quantify_scheduler/backends/types/qblox.py
@@ -187,7 +187,6 @@ class PulsarRFSettings(PulsarSettings):
             lo0_freq: float = complex_output_0.get("lo_freq")
         if complex_output_1:
             lo1_freq: float = complex_output_1.get("lo_freq")
-        lo1_freq: float = mapping.get("lo1_freq")
 
 
         return PulsarRFSettings(ref=ref, lo0_freq=lo0_freq, lo1_freq=lo1_freq)
