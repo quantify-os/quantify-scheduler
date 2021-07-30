795e82b6027e3c069e9781017e8bb670bedbe4c1
Diogo Valada <dvalada@qblox.com>
2021-07-30 12:28:34 +0200

    Added RF component tests

--- tests/scheduler/instrument_coordinator/components/test_qblox.py
+++ tests/scheduler/instrument_coordinator/components/test_qblox.py
@@ -81,6 +81,63 @@ def make_qrm(mocker):
 
     yield _make_qrm
 
+@pytest.fixture
+def make_qcm_rf(mocker):
+    def _make_qcm_rf(
+        name: str = "qcm_rf0", serial: str = "dummy"
+    ) -> qblox.PulsarQCMRFComponent:
+        mocker.patch("qcodes.instrument.Instrument.record_instance")
+
+        class pulsar_qcm_rf(pulsar_qcm.pulsar_qcm_qcodes):
+            def _get_lo_hw_present(self):
+                return True
+
+        qcm_rf: pulsar_qcm_rf = mocker.create_autospec(
+            pulsar_qcm_rf, instance=True
+        )
+        qcm_rf.name = name
+        qcm_rf._serial = serial
+
+        component = qblox.PulsarQCMRFComponent(qcm_rf)
+        mocker.patch.object(component.instrument_ref, "get_instr", return_value=qcm_rf)
+        mocker.patch.object(
+            component.instrument,
+            "get_sequencer_state",
+            return_value={"status": "ARMED"},
+        )
+
+        return component
+
+    yield _make_qcm_rf
+
+@pytest.fixture
+def make_qrm_rf(mocker):
+    def _make_qrm_rf(
+        name: str = "qrm_rf0", serial: str = "dummy"
+    ) -> qblox.PulsarQRMRFComponent:
+        mocker.patch("qcodes.instrument.Instrument.record_instance")
+
+        class pulsar_qrm_rf(pulsar_qrm.pulsar_qrm_qcodes):
+            def _get_lo_hw_present(self):
+                return True
+
+        qrm_rf: pulsar_qrm_rf = mocker.create_autospec(
+            pulsar_qrm_rf, instance=True
+        )
+        qrm_rf.name = name
+        qrm_rf._serial = serial
+
+        component = qblox.PulsarQRMRFComponent(qrm_rf)
+        mocker.patch.object(component.instrument_ref, "get_instr", return_value=qrm_rf)
+        mocker.patch.object(
+            component.instrument,
+            "get_sequencer_state",
+            return_value={"status": "ARMED"},
+        )
+
+        return component
+
+    yield _make_qrm_rf
 
 def test_initialize_pulsar_qcm_component(make_qcm):
     make_qcm("qblox_qcm0", "1234")
@@ -88,6 +145,13 @@ def test_initialize_pulsar_qcm_component(make_qcm):
 
 def test_initialize_pulsar_qrm_component(make_qrm):
     make_qrm("qblox_qrm0", "1234")
+
+def test_initialize_pulsar_qcm_rf_component(make_qcm_rf):
+    make_qcm_rf("qblox_qcm_rf0", "1234")
+
+
+def test_initialize_pulsar_qrm_rf_component(make_qrm_rf):
+    make_qrm_rf("qblox_qrm_rf0", "1234")
 
 
 def test_prepare(schedule_with_measurement, make_qcm, make_qrm):
@@ -106,7 +170,25 @@ def test_prepare(schedule_with_measurement, make_qcm, make_qrm):
 
     # Assert
     qcm.instrument.arm_sequencer.assert_called_with(sequencer=0)
+    qrm.instrument.arm_sequencer.assert_called_with(sequencer=0)
+
+def test_prepare_rf(schedule_with_measurement, make_qcm_rf, make_qrm_rf):
+    # Arrange
+    qcm: qblox.PulsarQCMRFComponent = make_qcm_rf("qcm_rf0", "1234")
+    qrm: qblox.PulsarQRMRFComponent = make_qrm_rf("qrm_rf0", "1234")
+
+    # Act
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        set_datadir(tmp_dir)
+
+        prog = qcompile(schedule_with_measurement, DEVICE_CFG, HARDWARE_MAPPING)
+
+        qcm.prepare(prog["qcm_rf0"])
+        qrm.prepare(prog["qrm_rf0"])
+
+    # Assert
     qcm.instrument.arm_sequencer.assert_called_with(sequencer=0)
+    qrm.instrument.arm_sequencer.assert_called_with(sequencer=0)
 
 
 def test_prepare_exception_qcm(make_qcm):
@@ -129,6 +211,39 @@ def test_prepare_exception_qcm(make_qcm):
 def test_prepare_exception_qrm(make_qrm):
     # Arrange
     qrm: qblox.PulsarQRMComponent = make_qrm("qcm0", "1234")
+
+    invalid_config = {"idontexist": "this is not used"}
+
+    # Act
+    with pytest.raises(KeyError) as execinfo:
+        qrm.prepare(invalid_config)
+
+    # Assert
+    assert execinfo.value.args[0] == (
+        "Invalid program. Attempting to access non-existing sequencer with"
+        ' name "idontexist".'
+    )
+
+def test_prepare_exception_qcm_rf(make_qcm_rf):
+    # Arrange
+    qcm: qblox.PulsarQCMComponent = make_qcm_rf("qcm_rf0", "1234")
+
+    invalid_config = {"idontexist": "this is not used"}
+
+    # Act
+    with pytest.raises(KeyError) as execinfo:
+        qcm.prepare(invalid_config)
+
+    # Assert
+    assert execinfo.value.args[0] == (
+        "Invalid program. Attempting to access non-existing sequencer with"
+        ' name "idontexist".'
+    )
+
+
+def test_prepare_exception_qrm_rf(make_qrm_rf):
+    # Arrange
+    qrm: qblox.PulsarQRMComponent = make_qrm_rf("qcm_rf0", "1234")
 
     invalid_config = {"idontexist": "this is not used"}
 
@@ -171,6 +286,33 @@ def test_retrieve_acquisition_qrm(schedule_with_measurement, make_qrm):
     # Assert
     assert len(acq) == 2
 
+def test_retrieve_acquisition_qcm_rf(make_qcm_rf):
+    # Arrange
+    qcm_rf: qblox.PulsarQCMRFComponent = make_qcm_rf("qcm_rf0", "1234")
+
+    # Act
+    acq = qcm_rf.retrieve_acquisition()
+
+    # Assert
+    assert acq is None
+
+
+def test_retrieve_acquisition_qrm_rf(schedule_with_measurement, make_qrm_rf):
+    # Arrange
+    qrm_rf: qblox.PulsarQRMComponent = make_qrm_rf("qcm_rf0", "1234")
+
+    # Act
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        set_datadir(tmp_dir)
+        prog = qcompile(schedule_with_measurement, DEVICE_CFG, HARDWARE_MAPPING)
+        prog = dict(prog)
+
+        qrm_rf.prepare(prog[qrm_rf.instrument.name])
+        qrm_rf.start()
+        acq = qrm_rf.retrieve_acquisition()
+
+    # Assert
+    assert len(acq) == 2
 
 def test_start_qcm_qrm(schedule_with_measurement, make_qcm, make_qrm):
     # Arrange
@@ -193,6 +335,27 @@ def test_start_qcm_qrm(schedule_with_measurement, make_qcm, make_qrm):
     qcm.instrument.start_sequencer.assert_called()
     qrm.instrument.start_sequencer.assert_called()
 
+def test_start_qcm_qrm_rf(schedule_with_measurement, make_qcm_rf, make_qrm_rf):
+    # Arrange
+    qcm_rf: qblox.PulsarQCMRFComponent = make_qcm_rf("qcm_rf0", "1234")
+    qrm_rf: qblox.PulsarQRMRFComponent = make_qrm_rf("qrm_rf0", "1234")
+
+    # Act
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        set_datadir(tmp_dir)
+
+        prog = qcompile(schedule_with_measurement, DEVICE_CFG, HARDWARE_MAPPING)
+
+        qcm_rf.prepare(prog["qcm0"])
+        qrm_rf.prepare(prog["qrm0"])
+
+        qcm_rf.start()
+        qrm_rf.start()
+
+    # Assert
+    qcm_rf.instrument.start_sequencer.assert_called()
+    qrm_rf.instrument.start_sequencer.assert_called()
+
 
 def test_stop_qcm_qrm(make_qcm, make_qrm):
     # Arrange
@@ -206,6 +369,19 @@ def test_stop_qcm_qrm(make_qcm, make_qrm):
     # Assert
     qcm.instrument.stop_sequencer.assert_called()
     qrm.instrument.stop_sequencer.assert_called()
+
+def test_stop_qcm_qrm_rf(make_qcm, make_qrm):
+    # Arrange
+    qcm_rf: qblox.PulsarQCMRFComponent = make_qcm("qcm_rf0", "1234")
+    qrm_rf: qblox.PulsarQRMRFComponent = make_qrm("qrm_rf0", "1234")
+
+    # Act
+    qcm_rf.stop()
+    qrm_rf.stop()
+
+    # Assert
+    qcm_rf.instrument.stop_sequencer.assert_called()
+    qrm_rf.instrument.stop_sequencer.assert_called()
 
 
 def test_demodulate_trace():
