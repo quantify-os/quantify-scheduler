a2f22450a8778a0db6121112d1f3224c58d2d50e
Viacheslav Ostroukh <dev@slavouti.ch>
2021-08-17 13:32:16 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- quantify_scheduler/types.py
+++ quantify_scheduler/types.py
@@ -425,7 +425,7 @@ class ScheduleBase(JSONSchemaValMixin, UserDict, ABC):
         on the reference operation (:code:`"ref_op"`) and a reference point on the next
         added operation (:code:`"ref_pt_new"').
         A reference point can be either the "start", "center", or "end" of an
-        operations. The reference operation (:code:`"ref_op"`) is specified using its
+        operation. The reference operation (:code:`"ref_op"`) is specified using its
         label property.
 
         Each item in the list represents a timing constraint and is a dictionary with
