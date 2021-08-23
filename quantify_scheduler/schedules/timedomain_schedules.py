8c370a647f3e88c6db66b67d4ca4428f6b657911
adriaanrol <adriaan.rol@gmail.com>
2021-08-23 12:34:02 +0200

    undo spurious change to AllXY schedule

--- quantify_scheduler/schedules/timedomain_schedules.py
+++ quantify_scheduler/schedules/timedomain_schedules.py
@@ -254,7 +254,7 @@ def echo_sched(
 
 def allxy_sched(
     qubit: str,
-    element_select_idx: Union[Literal["All"], int] = "All",
+    element_select_idx: Union[np.ndarray, int] = np.arange(21),
     repetitions: int = 1,
 ) -> Schedule:
     # pylint: disable=line-too-long
@@ -275,8 +275,7 @@ def allxy_sched(
     qubit
         the name of the qubit e.g., :code:`"q0"` to perform the experiment on.
     element_select_idx
-        the index of the particular element of the AllXY experiment to exectute -
-        or :code:`"All"` for all elemements of the sequence.
+        the index of the particular element of the AllXY experiment to exectute.
     repetitions
         The amount of times the Schedule will be repeated.
 
@@ -338,7 +337,8 @@ def allxy_sched(
 def readout_calibration_sched(
     qubit: str,
     prepared_states: List[int],
-    repetitions: int = 1,)->Schedule:
+    repetitions: int = 1,
+) -> Schedule:
     """
     A schedule for readout calibration. Prepares a state and immediately performs
     a measurement.
@@ -357,10 +357,12 @@ def readout_calibration_sched(
             raise NotImplementedError()
         else:
             raise ValueError(f"Prepared state ({prep_state})must be either 0, 1 or 2")
-        schedule.add(Measure(qubit, acq_index=i, bin_mode=BinMode.APPEND), label=f"Measurement {i}")
+        schedule.add(
+            Measure(qubit, acq_index=i, bin_mode=BinMode.APPEND),
+            label=f"Measurement {i}",
+        )
 
     return schedule
-
 
 
 # pylint: disable=too-many-arguments
