8561fcfbd6543653efc84846a8935daf831f136b
Damien <damiencrielaard@gmail.com>
2021-09-09 11:10:47 +0200

    docstrings

--- quantify_scheduler/backends/qblox/constants.py
+++ quantify_scheduler/backends/qblox/constants.py
@@ -18,17 +18,25 @@ Clock period of the sequencers. All time intervals used must be multiples of thi
 MIN_TIME_BETWEEN_ACQUISITIONS = 1000  # ns
 """Minimum time between two acquisitions to prevent FIFO errors."""
 SAMPLING_RATE = 1_000_000_000  # 1GS/s
-"""Sampling rate of the Qblox Pulsar series instruments."""
+"""Sampling rate of the Qblox control/readout instruments."""
 PULSE_STITCHING_DURATION = 1e-6
 """Duration of the individual pulses when pulse stitching is used."""
 NUMBER_OF_SEQUENCERS_QCM = 2
-"""Number of sequencers supported by Pulsar QCM in the latest firmware."""
+"""Number of sequencers supported by a QCM in the latest firmware."""
 NUMBER_OF_SEQUENCERS_QRM = 1
-"""Number of sequencers supported by Pulsar QRM in the latest firmware."""
+"""Number of sequencers supported by a QRM in the latest firmware."""
 MIN_MIXER_PHASE_ERROR = -45
+"""Lowest phase shift that can be configured in the instruments for mixer corrections.
+"""
 MAX_MIXER_PHASE_ERROR = 45
+"""Lowest phase shift that can be configured in the instruments for mixer corrections.
+"""
 MIN_MIXER_AMP_RATIO = 0.5
+"""Lowest value the amplitude correction can be set to. N.B. This correction is defined
+as Q/I."""
 MAX_MIXER_AMP_RATIO = 2.0
+"""Highest value the amplitude correction can be set to. N.B. This correction is defined
+as Q/I."""
 MIN_MIXER_DC_OFFSET = -1.0
 """Lowest value the dc dac offset can have. This value is normalized w.r.t. the output
 range."""
