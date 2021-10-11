18ed8939f91c46d65d6b3b1f45495db5843d931c
Damien <damiencrielaard@gmail.com>
2021-10-11 14:22:28 +0200

    moved version check to separate module

---
+++ quantify_scheduler/backends/qblox/driver_version_check.py
@@ -0,0 +1,46 @@
+# Repository: https://gitlab.com/quantify-os/quantify-scheduler
+# Licensed according to the LICENCE file on the master branch
+"""Helper functions to perform the version check for qblox_instruments."""
+
+try:
+    from qblox_instruments.build import __version__ as driver_version
+except ImportError:
+    driver_version = None
+
+SUPPORTED_DRIVER_VERSIONS = ("0.5.0", "0.5.1", "0.5.2")
+
+
+class DriverVersionError(Exception):
+    """
+    Raise when the installed driver version is not supported
+    """
+
+
+def verify_qblox_instruments_version(version=driver_version):
+    """
+    Verifies whether the installed version is supported by the qblox_backend.
+
+    Raises
+    ------
+    DriverVersionError
+        When an incorrect or no installation of qblox-instruments was found.
+    """
+    if version is None:
+        raise DriverVersionError(
+            "Qblox DriverVersionError: qblox-instruments version check could not be "
+            "performed. Either the package is not installed "
+            "correctly or a version < 0.3.2 was found."
+        )
+    if version not in SUPPORTED_DRIVER_VERSIONS:
+        message = (
+            f"Qblox DriverVersionError: Installed driver version {version}"
+            f" not supported by backend."
+        )
+        message += (
+            f" Please install version {SUPPORTED_DRIVER_VERSIONS[0]}"
+            if len(SUPPORTED_DRIVER_VERSIONS) == 1
+            else f" Please install a supported version (currently supported: "
+            f"{SUPPORTED_DRIVER_VERSIONS})"
+        )
+        message += " to continue to use this backend."
+        raise DriverVersionError(message)
