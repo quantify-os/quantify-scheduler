00c66db9ab4735ad8d5fe4c59349f9976f0dbc40
Pieter Eendebak <pieter.eendebak@gmail.com>
2021-11-15 07:55:39 +0100

    fix t0 info

--- quantify_scheduler/visualization/pulse_diagram.py
+++ quantify_scheduler/visualization/pulse_diagram.py
@@ -528,7 +528,7 @@ def plot_acquisition_operations(
     for idx, tc in enumerate(schedule.timing_constraints):
         operation = schedule.operations[tc["operation_repr"]]
         if isinstance(operation, AcquisitionOperation):
-            t0 = tc["abs_time"] + pulse_info["t0"]
+            t0 = tc["abs_time"] + operation.data["acquisition_info"][0]["t0"]
             t1 = t0 + operation.duration
             h = ax.axvspan(t0, t1, **kwargs)
             handles.append(h)
