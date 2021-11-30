8c5a1e07b11fdf898bc4b0e79deca4927742b05b
Damien <damiencrielaard@gmail.com>
2021-11-30 10:17:33 +0100

    add_to_wf_dict_if_unique

--- quantify_scheduler/backends/qblox/operation_handling/base.py
+++ quantify_scheduler/backends/qblox/operation_handling/base.py
@@ -49,3 +49,21 @@ def get_indices_from_wf_dict(uuid: str, wf_dict: Dict[str, Any]) -> Tuple[int, i
     idx_real = None if name_real not in wf_dict else wf_dict[name_real]["index"]
     idx_imag = None if name_imag not in wf_dict else wf_dict[name_imag]["index"]
     return idx_real, idx_imag
+
+
+def add_to_wf_dict_if_unique(
+    wf_dict: Dict[str, Any], waveform: np.ndarray
+) -> Tuple[Dict[str, Any], str, int]:
+    def generate_entry(name: str, data: np.ndarray, idx: int) -> Dict[str, Any]:
+        return {name: {"data": data.tolist(), "index": idx}}
+
+    if not np.isrealobj(waveform):
+        raise RuntimeError(f"This function only accepts real arrays.")
+
+    uuid = helpers.generate_uuid_from_wf_data(waveform)
+    if uuid in wf_dict:
+        index: int = wf_dict[uuid]["index"]
+    else:
+        index = len(wf_dict)
+        wf_dict.update(generate_entry(uuid, waveform, len(wf_dict)))
+    return wf_dict, uuid, index
