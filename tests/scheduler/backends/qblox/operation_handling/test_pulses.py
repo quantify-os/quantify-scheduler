1039d9b7e65649c837c67e38718dc97e63f67349
Damien <damiencrielaard@gmail.com>
2021-12-07 14:03:59 +0100

    copyright header

--- tests/scheduler/backends/qblox/operation_handling/test_pulses.py
+++ tests/scheduler/backends/qblox/operation_handling/test_pulses.py
@@ -1,3 +1,7 @@
+# Repository: https://gitlab.com/quantify-os/quantify-scheduler
+# Licensed according to the LICENCE file on the master branch
+"""Tests for pulses module."""
+
 import pytest
 import numpy as np
 
