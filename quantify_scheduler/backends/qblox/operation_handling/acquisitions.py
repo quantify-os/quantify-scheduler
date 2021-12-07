b3dfe26a71f5ac69bb8f76371b82f97b8aeae496
Damien <damiencrielaard@gmail.com>
2021-12-07 14:04:20 +0100

    copyright header

--- quantify_scheduler/backends/qblox/operation_handling/acquisitions.py
+++ quantify_scheduler/backends/qblox/operation_handling/acquisitions.py
@@ -1,3 +1,7 @@
+# Repository: https://gitlab.com/quantify-os/quantify-scheduler
+# Licensed according to the LICENCE file on the master branch
+"""Classes for handling acquisitions."""
+
 from __future__ import annotations
 
 from typing import Optional, Dict, Any, Union
