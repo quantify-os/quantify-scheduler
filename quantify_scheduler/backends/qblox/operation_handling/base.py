e12c7f30f77b33748cf491af9cebc7ad5f0efa0e
Damien <damiencrielaard@gmail.com>
2021-12-08 16:04:13 +0100

    added docstrings

--- quantify_scheduler/backends/qblox/operation_handling/base.py
+++ quantify_scheduler/backends/qblox/operation_handling/base.py
@@ -1,6 +1,6 @@
 # Repository: https://gitlab.com/quantify-os/quantify-scheduler
 # Licensed according to the LICENCE file on the master branch
-"""defines interfaces for operation handling strategies."""
+"""Defines interfaces for operation handling strategies."""
 
 from __future__ import annotations
 
@@ -14,15 +14,34 @@ from quantify_scheduler.backends.qblox.qasm_program import QASMProgram
 
 
 class IOperationStrategy(ABC):
+    """Defines the interface operation strategies must adhere to."""
+
     @property
     @abstractmethod
     def operation_info(self) -> types.OpInfo:
-        pass
+        """Returns the pulse/acquisition information extracted from the schedule."""
 
     @abstractmethod
     def generate_data(self, wf_dict: Dict[str, Any]):
-        pass
+        """
+        Generates the waveform data and adds them to the wf_dict (if not already
+        present). This is either the awg data, or the acquisition weights.
+
+        Parameters
+        ----------
+        wf_dict
+            The dictionary to add the waveform to. N.B. the dictionary is modified in
+            function.
+        """
 
     @abstractmethod
     def insert_qasm(self, qasm_program: QASMProgram):
-        pass
+        """
+        Add the assembly instructions for the Q1 sequence processor that corresponds to
+        this pulse/acquisition.
+
+        Parameters
+        ----------
+        qasm_program
+            The QASMProgram to add the assembly instructions to.
+        """
