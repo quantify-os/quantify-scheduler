e3dcf02a663e201f554b6aed5b7efd937013356c
Damien <damiencrielaard@gmail.com>
2021-12-08 15:07:46 +0100

    docstring

--- quantify_scheduler/backends/qblox/operation_handling/factory.py
+++ quantify_scheduler/backends/qblox/operation_handling/factory.py
@@ -4,8 +4,6 @@
 
 from __future__ import annotations
 
-from typing import Dict, Any
-
 from quantify_scheduler.enums import BinMode
 
 from quantify_scheduler.backends.types.qblox import OpInfo
@@ -19,6 +17,25 @@ from quantify_scheduler.backends.qblox.operation_handling import (
 def get_operation_strategy(
     operation: OpInfo, instruction_generated_pulses_enabled: bool, output_mode: str
 ) -> base.IOperationStrategy:
+    """
+    Determines and instantiates the correct strategy object.
+
+    Parameters
+    ----------
+    operation
+        The operation we are building the strategy for.
+    instruction_generated_pulses_enabled
+        Specifies if instruction generated pulses (e.g. staircase through offsets) are
+        allowed. If set to False, only generically treated pulses are allowed.
+    output_mode
+        Either "real", "imag" or complex depending on whether the signal affects only
+        path0, path1 or both.
+
+    Returns
+    -------
+    :
+        The instantiated strategy object.
+    """
     if operation.is_acquisition:
         return _get_acquisition_strategy(operation)
     return _get_pulse_strategy(
@@ -29,6 +46,7 @@ def get_operation_strategy(
 def _get_acquisition_strategy(
     operation: OpInfo,
 ) -> acquisitions.AcquisitionStrategyPartial:
+    """Handles the logic for determining the correct acquisition type."""
     protocol = operation.data["protocol"]
     if protocol in ("trace", "ssb_integration_complex"):
         if protocol == "trace" and operation.data["bin_mode"] == BinMode.APPEND.value:
@@ -48,6 +66,7 @@ def _get_acquisition_strategy(
 def _get_pulse_strategy(
     operation: OpInfo, instruction_generated_pulses_enabled: bool, output_mode: str
 ) -> base.IOperationStrategy:
+    """Handles the logic for determining the correct pulse type."""
     if instruction_generated_pulses_enabled:
         wf_func = operation.data["wf_func"]
         if wf_func == "quantify_scheduler.waveforms.square":
