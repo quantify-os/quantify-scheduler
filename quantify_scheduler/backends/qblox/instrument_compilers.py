b494ebba614471cb7f3c3140435ba82dd7ebc527
Damien <damiencrielaard@gmail.com>
2022-01-31 16:22:40 +0100

    add init marker config

--- quantify_scheduler/backends/qblox/instrument_compilers.py
+++ quantify_scheduler/backends/qblox/instrument_compilers.py
@@ -136,7 +136,7 @@ class QcmModule(compiler_abc.QbloxBasebandModule):
         instrument_type="QCM",
         max_sequencers=NUMBER_OF_SEQUENCERS_QCM,
         max_awg_output_voltage=2.5,
-        marker_configuration=MarkerConfiguration(start=0b1111, end=0b0000),
+        marker_configuration=MarkerConfiguration(init=None, start=0b1111, end=0b0000),
         mixer_dc_offset_range=BoundedParameter(min_val=-2.5, max_val=2.5, units="V"),
     )
 
@@ -152,7 +152,7 @@ class QrmModule(compiler_abc.QbloxBasebandModule):
         instrument_type="QRM",
         max_sequencers=NUMBER_OF_SEQUENCERS_QRM,
         max_awg_output_voltage=0.5,
-        marker_configuration=MarkerConfiguration(start=0b1111, end=0b0000),
+        marker_configuration=MarkerConfiguration(init=None, start=0b1111, end=0b0000),
         mixer_dc_offset_range=BoundedParameter(min_val=-0.5, max_val=0.5, units="V"),
     )
 
@@ -167,7 +167,7 @@ class QcmRfModule(compiler_abc.QbloxRFModule):
         instrument_type="QCM-RF",
         max_sequencers=NUMBER_OF_SEQUENCERS_QCM,
         max_awg_output_voltage=0.25,
-        marker_configuration=MarkerConfiguration(start=0b1111, end=0b0000),
+        marker_configuration=MarkerConfiguration(init=0b0011, start=0b1111, end=0b0000),
         mixer_dc_offset_range=BoundedParameter(min_val=-50, max_val=50, units="mV"),
     )
 
@@ -182,7 +182,7 @@ class QrmRfModule(compiler_abc.QbloxRFModule):
         instrument_type="QRM-RF",
         max_sequencers=NUMBER_OF_SEQUENCERS_QRM,
         max_awg_output_voltage=0.25,
-        marker_configuration=MarkerConfiguration(start=0b1111, end=0b0000),
+        marker_configuration=MarkerConfiguration(init=0b0011, start=0b1111, end=0b0000),
         mixer_dc_offset_range=BoundedParameter(min_val=-50, max_val=50, units="mV"),
     )
 
