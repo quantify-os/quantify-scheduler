ee63e49d90b9ec2e570542d111772ae8aca5a9cd
Damaz de Jong <ddejong@qblox.com>
2022-04-08 15:49:55 +0200

    incorporate the comments by Fokko

--- docs/tutorials/Tutorial 5. Operations and Qubits.rst.py
+++ docs/tutorials/Tutorial 5. Operations and Qubits.rst.py
@@ -7,9 +7,9 @@
 #       format_version: '1.3'
 #       jupytext_version: 1.13.4
 #   kernelspec:
-#     display_name: Python [conda env:pysweep-env]
+#     display_name: Python 3 (ipykernel)
 #     language: python
-#     name: conda-env-pysweep-env-py
+#     name: python3
 # ---
 
 # %% [raw]
@@ -37,7 +37,9 @@
 # operations are defined in terms of explicit signals and locations on chip, rather than the qubit and the intended operation.
 # To allow working at a greater level of abstraction, `quantify_scheduler` allows creating operations on the
 # :ref:`quantum-circuit level<sec-user-guide-quantum-circuit>`.
-# Instead of signals, clocks, and ports, operations are defined by the the effect they have on specific qubits.
+# Instead of signals, clocks, and ports, operations are defined by the the effect they have on specific qubits. This representation of the schedules can be compiled to the quantum-device level to create the pulse schemes.
+#
+# In this tutorial we show how to define operations on the :ref:`quantum-circuit level<sec-user-guide-quantum-circuit>`, combine them into schedules, and show their circuit level visualization. We go through the configuration file needed to compile the schedule to the quantum-device level and show how these configuration files can be created automatically and dynamically. Finally, we showcase the hybrid nature of quantify-scheduler, allowing the scheduling circuit level and device level operations side by side in the same schedule.
 #
 # Many of the gates used in the circuit layer description are defined in
 # :class:`quantify_scheduler.operations.gate_library` such as `Reset`, `X90` and
@@ -52,7 +54,7 @@ q0, q1 = ("q0", "q1")
 X90(q0)
 Measure(q1)
 CZ(q0, q1)
-Reset(q0)
+Reset(q0);
 
 # %% [raw]
 # Let's investigate the different components present in the circuit level description of
@@ -95,13 +97,13 @@ pprint(operation_schema["properties"]["gate_info"]["properties"])
 # We exemplify this extra layer of abstraction by creating a `schedule` for measuring
 # `Bell violations`.
 #
-# .. note:: Within a single `schedule`, high-level circuit layer operations can be mixed with pulse level operations. This mixed representation is useful for experiments where some pulses cannot easily be represented as qubit gates. An example of this is given by the `Chevron` experiment given in :ref:`Mixing pulse and circuit layer operations <Mixing pulse and circuit layer operations>`.
+# .. note:: Within a single `schedule`, high-level circuit layer operations can be mixed with quantum-device level operations. This mixed representation is useful for experiments where some pulses cannot easily be represented as qubit gates. An example of this is given by the `Chevron` experiment given in :ref:`Mixing pulse and circuit layer operations <Mixing pulse and circuit layer operations>`.
 
 # %% [raw]
 # As the first example, we want to create a schedule for performing the
 # `Bell experiment <https://en.wikipedia.org/wiki/Bell%27s_theorem>`_.
 # The goal of the Bell experiment is to create a Bell state
-# :math:`|\Phi ^+\rangle=\frac{1}{2}(|00\rangle+|11\rangle)` followed by a measurement.
+# :math:`|\Phi ^+\rangle=\frac{1}{2}(|00\rangle+|11\rangle)` which is a perfectly entagled state, followed by a measurement.
 # By rotating the measurement basis, or equivalently one of the qubits, it is possible
 # to observe violations of the CSHS inequality.
 #
@@ -117,7 +119,6 @@ from quantify_scheduler import Schedule
 
 sched = Schedule("Bell experiment")
 
-# we use a regular for loop as we have to unroll the changing theta variable here
 for acq_idx, theta in enumerate(np.linspace(0, 360, 21)):
     sched.add(Reset(q0, q1))
     sched.add(X90(q0))
@@ -135,7 +136,7 @@ for acq_idx, theta in enumerate(np.linspace(0, 360, 21)):
 sched
 
 # %% [raw]
-# By scheduling 7 operations for 21 different values for `theta` we indeed get a schedule containing 7\*21=147 operations. To minimize the size of the schedule, identical operations are stored only once. For example, all 21 `CZ` operations are stored only once, which leaves only 66 unique operations in the schedule.
+# By scheduling 7 operations for 21 different values for `theta` we indeed get a schedule containing 7\*21=147 operations. To minimize the size of the schedule, identical operations are stored only once. For example, the `CZ` operations is stored only once but used 21 times, which leaves only 66 unique operations in the schedule.
 # .. note :: The acquisitions are different for every iteration due to their different `acq_index`. The `Rxy` rotates over a different angle every iteration and must therefore also be different for every iteration (except for the last since $R^{360}=R^0$). Hence the number of unique operations is 3\*21-1+4=66.
 
 # %% [raw]
@@ -247,6 +248,9 @@ pprint(transmon_schema["properties"])
 # %%
 pprint(list(transmon_test_config["qubits"].keys()))
 
+# %% [raw]
+# In fact, this configuration file specifies more qubits than we need for our schedule. The unused qubit will be ignored. This allows writing only a single configuration file for separate experiments using a different number of qubits.
+
 # %%
 # For every qubit we can investigate the contained parameters
 pprint(transmon_test_config["qubits"]["q0"])
@@ -281,9 +285,7 @@ ax.set_xlim(0.4005e-3, 0.4006e-3)
 # Quantum Devices and Elements
 # ----------------------------
 # The :ref:`device configuration file<sec-device-config>` contains all knowledge
-# of the physical device under test (DUT). Together with the
-# :ref:`hardware configuration file<Hardware configuration file>` which contains the
-# different hardware connections, all information is represented.
+# of the physical device under test (DUT).
 # To generate these configuration files on the fly, `quantify_scheduler` provides the
 # :class:`~quantify_scheduler.device_under_test.quantum_device.QuantumDevice` and
 # :code:`DeviceElement` classes.
@@ -293,7 +295,7 @@ ax.set_xlim(0.4005e-3, 0.4006e-3)
 # represents the DUT containing different :code:`DeviceElement` s.
 # Currently, `quantify_scheduler` contains the
 # :class:`~quantify_scheduler.device_under_test.transmon_element.TransmonElement` class
-# to represent a qubit connected to a feedline. We show their interaction below:
+# to represent a transmon qubit connected to a feedline. We show their interaction below:
 
 # %%
 from quantify_scheduler.device_under_test.transmon_element import TransmonElement
@@ -395,9 +397,7 @@ dut.add_component(q0)
 dut.add_component(q1)
 dut.get_component("q0").mw_amp180(0.6)
 dut.get_component("q1").mw_amp180(0.6)
-compiled_sched = qcompile(
-    sched, dut.generate_device_config(), dut.generate_hardware_config()
-)
+compiled_sched = qcompile(sched, dut.generate_device_config())
 
 # %% [raw]
 # So, finally, we can show the timing table associated to the chevron schedule and plot
