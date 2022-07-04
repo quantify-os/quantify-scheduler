f8bc0951be1ca07a9a1020a1827f646d01da7bfe
adriaanrol <adriaan.rol@gmail.com>
2022-07-04 16:12:53 +0200

    changed defaults and docstring for transmon element, relates to #306

--- quantify_scheduler/device_under_test/transmon_element.py
+++ quantify_scheduler/device_under_test/transmon_element.py
@@ -195,11 +195,16 @@ class DispersiveMeasurement(InstrumentChannel):
 
         self.add_parameter(
             "acq_delay",
-            docstring="Delay between the start of the readout pulse and the start of the acquisition.",
-            initial_value=0,
+            docstring="Delay between the start of the readout pulse and the start of "
+            + "the acquisition. Note that some hardware backends do not support "
+            "starting a pulse and the acquisition in the same clock cycle making 0 "
+            "delay an invalid value.",
+            initial_value=0,  # float("nan"),
             unit="s",
             parameter_class=ManualParameter,
-            vals=validators.Numbers(min_value=0, max_value=1),
+            # in principle the values should be a few 100 ns but the validator is here
+            # only to protect against silly typo's that lead to out of memory errors.
+            vals=validators.Numbers(min_value=0, max_value=100e-6),
         )
         self.add_parameter(
             "integration_time",
@@ -207,7 +212,9 @@ class DispersiveMeasurement(InstrumentChannel):
             initial_value=1e-6,
             unit="s",
             parameter_class=ManualParameter,
-            vals=validators.Numbers(min_value=0, max_value=1),
+            # in principle the values should be a few us but the validator is here
+            # only to protect against silly typo's that lead to out of memory errors.
+            vals=validators.Numbers(min_value=0, max_value=100e-6),
         )
 
         ro_acq_weight_type_validator = validators.Enum("SSB")
