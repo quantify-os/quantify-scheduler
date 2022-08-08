4f7fbdf079a7fedf51220ddb46d1a2b56f740696
Edgar Reehuis <10068237-ereehuis@users.noreply.gitlab.com>
2022-08-08 14:51:32 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- quantify_scheduler/json_utils.py
+++ quantify_scheduler/json_utils.py
@@ -165,7 +165,7 @@ class ScheduleJSONDecoder(json.JSONDecoder):
         ] + extended_modules
         self.classes = inspect_helpers.get_classes(*self._modules)
         self.classes.update(
-            {"Schedulable": Schedulable, "AcquisitionMetadata": AcquisitionMetadata}
+            {AcquisitionMetadata.__name__: AcquisitionMetadata, Schedulable.__name__: Schedulable}
         )
 
     def decode_dict(self, obj: Dict[str, Any]) -> Dict[str, Any]:
