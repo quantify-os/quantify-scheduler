8e6da839aa66c836d3a6694c25248143b2cdc303
Edgar Reehuis <10068237-ereehuis@users.noreply.gitlab.com>
2022-08-10 12:40:25 +0200

    Trying to shut up Codacy

--- quantify_scheduler/backends/__init__.py
+++ quantify_scheduler/backends/__init__.py
@@ -1,3 +1,5 @@
+# Repository: https://gitlab.com/quantify-os/quantify-scheduler
+# Licensed according to the LICENCE file on the main branch
 from .graph_compilation import SerialCompiler
 
 __all__ = [
