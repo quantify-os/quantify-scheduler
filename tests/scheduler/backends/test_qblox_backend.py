8d8f2cf7cba8f59269224d4e2372b516a4050541
Davidrobertquinn <dquinn@qblox.com>
2022-12-12 17:26:23 +0100

    Refactored qcompile with SerialCompiler

--- tests/scheduler/backends/test_qblox_backend.py
+++ tests/scheduler/backends/test_qblox_backend.py
@@ -869,23 +869,25 @@ def test_compile_cluster(
     )
 
 
-def test_compile_no_device_cfg(load_example_qblox_hardware_config):
+def test_compile_no_device_cfg(
+    mock_setup_basic_transmon, load_example_qblox_hardware_config
+):
 
     sched = Schedule("One pulse schedule")
     sched.add_resources([ClockResource("q0.01", 3.1e9)])
     sched.add(SquarePulse(amp=1 / 4, duration=12e-9, port="q0:mw", clock="q0.01"))
-
-    compiled_schedule = qcompile(
-        schedule=sched, hardware_cfg=load_example_qblox_hardware_config
+    quantum_device = mock_setup_basic_transmon["quantum_device"]
+    quantum_device.hardware_config(load_example_qblox_hardware_config)
+    compiler = SerialCompiler(name="compiler")
+    compiled_schedule = compiler.compile(
+        sched, config=quantum_device.generate_compilation_config()
     )
-
     wf_and_prog = compiled_schedule.compiled_instructions["qcm0"]["seq0"]["sequence"]
     assert "play" in wf_and_prog["program"]
 
 
 def test_compile_simple_multiplexing(
     pulse_only_schedule_multiplexed,
-    load_example_transmon_config,
     hardware_cfg_multiplexing,
     mock_setup_basic_transmon,
 ):
