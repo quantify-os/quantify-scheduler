e40688516d5b9f614402a4defa99a4c92dab1735
davidvos99 <davidvos1999@gmail.com>
2022-12-23 17:13:43 +0100

    copy code from test branch

--- quantify_scheduler/backends/qblox/compiler_abc.py
+++ quantify_scheduler/backends/qblox/compiler_abc.py
@@ -559,6 +559,43 @@ class Sequencer:
             acq.generate_data(wf_dict)
         return wf_dict
 
+    def _prepare_acq_settings(self, acquisitions: List[IOperationStrategy]):
+        """ "
+        Sets sequencer settings that are specific to certain acquisitions.
+        For example for a TTL acquisition strategy
+
+        Parameters
+        ----------
+        acquisitions:
+            List of the acquisitions assigned to this sequencer.
+
+        Returns
+        ----------
+        -
+
+        """
+
+        acquisition_infos: List[OpInfo] = list(
+            map(lambda acq: acq.operation_info, acquisitions)
+        )
+        acq_metadata = _extract_acquisition_metadata_from_acquisitions(
+            acquisition_infos
+        )
+        if acq_metadata.acq_protocol == "trigger_count":
+            if acq_metadata.bin_mode == BinMode.AVERAGE:
+                self._settings.ttl_acq_auto_bin_incr_en = True
+            else:
+                self._settings.ttl_acq_auto_bin_incr_en = False
+            if self.connected_inputs is not None:
+                if len(self.connected_inputs) == 1:
+                    self._settings.ttl_acq_input_select = self.connected_inputs[0]
+                else:
+                    raise ValueError(
+                        f"Please make sure you use a single real input for this portclock combination. "
+                        f"Found: {len(self.connected_inputs)} connected. "
+                        "TTL acquisition does not support multiple inputs."
+                    )
+
     def _generate_acq_declaration_dict(
         self,
         acquisitions: List[IOperationStrategy],
@@ -620,7 +657,10 @@ class Sequencer:
             if acq_metadata.bin_mode == BinMode.APPEND:
                 num_bins = repetitions * (max(acq_indices) + 1)
             elif acq_metadata.bin_mode == BinMode.AVERAGE:
-                num_bins = max(acq_indices) + 1
+                if acq_metadata.acq_protocol == "trigger_count":
+                    num_bins = constants.MAX_NUMBER_OF_BINS
+                else:
+                    num_bins = max(acq_indices) + 1
             else:
                 # currently the BinMode enum only has average and append.
                 # this check exists to catch unexpected errors if we add more
@@ -904,16 +944,15 @@ class Sequencer:
         weights_dict = None
         acq_declaration_dict = None
         if self.parent.supports_acquisition:
-            weights_dict = (
-                self._generate_weights_dict() if len(self.acquisitions) > 0 else {}
-            )
-            acq_declaration_dict = (
-                self._generate_acq_declaration_dict(
+            if len(self.acquisitions) > 0:
+                self._prepare_acq_settings(self.acquisitions)
+                weights_dict = self._generate_weights_dict()
+                acq_declaration_dict = self._generate_acq_declaration_dict(
                     acquisitions=self.acquisitions, repetitions=repetitions
                 )
-                if len(self.acquisitions) > 0
-                else {}
-            )
+            else:
+                weights_dict = {}
+                acq_declaration_dict = {}
 
         qasm_program = self.generate_qasm_program(
             self.parent.total_play_time,
