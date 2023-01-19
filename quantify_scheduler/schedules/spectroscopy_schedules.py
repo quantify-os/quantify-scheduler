c3cc07d639ed71556b855b78490bf28e8526ee7c
Edgar Reehuis <10068237-ereehuis@users.noreply.gitlab.com>
2023-01-19 14:28:43 +0100

    Add nco_two_tone_spec_sched

--- quantify_scheduler/schedules/spectroscopy_schedules.py
+++ quantify_scheduler/schedules/spectroscopy_schedules.py
@@ -110,7 +110,7 @@ def nco_heterodyne_spec_sched(
     port_out: Optional[str] = None,
 ) -> Schedule:
     """
-    Generate a schedule for performing fast heterodyne spectroscopy using the NCO
+    Generate a **batched** schedule for performing fast heterodyne spectroscopy using the NCO.
 
     Parameters
     ----------
@@ -277,6 +277,117 @@ def two_tone_spec_sched(
         rel_time=ro_acquisition_delay,
         label="acquisition",
     )
+
+    return sched
+
+
+# pylint: disable=too-many-arguments
+# pylint: disable=too-many-locals
+def nco_two_tone_spec_sched(
+    spec_pulse_amp: float,
+    spec_pulse_duration: float,
+    spec_pulse_frequencies: np.ndarray,
+    spec_pulse_port: str,
+    spec_pulse_clock: str,
+    ro_pulse_amp: float,
+    ro_pulse_duration: float,
+    ro_pulse_delay: float,
+    ro_pulse_port: str,
+    ro_pulse_clock: str,
+    ro_pulse_frequency: float,
+    ro_acquisition_delay: float,
+    ro_integration_time: float,
+    init_duration: float = 10e-6,
+    repetitions: int = 1,
+) -> Schedule:
+    """
+    Generate a **batched** schedule for performing two-tone spectroscopy using NCO.
+
+    Parameters
+    ----------
+    spec_pulse_amp
+        amplitude of the spectroscopy pulse in Volt.
+    spec_pulse_duration
+        duration of the spectroscopy pulse in seconds.
+    spec_pulse_frequencies
+        frequencies of the spectroscopy pulse in Hertz.
+    spec_pulse_port
+        location on the device where the spectroscopy pulse should be applied.
+    spec_pulse_clock
+        reference clock used to track the spectroscopy frequency.
+    ro_pulse_amp
+        amplitude of the readout (spectroscopy) pulse in Volt.
+    ro_pulse_duration
+        duration of the readout (spectroscopy) pulse in seconds.
+    ro_pulse_delay
+        time between the end of the spectroscopy pulse and the start of the readout
+        (spectroscopy) pulse.
+    ro_pulse_port
+        location on the device where the readout (spectroscopy) pulse should be applied.
+    ro_pulse_clock
+        reference clock used to track the readout (spectroscopy) frequency.
+    ro_pulse_frequency
+        frequency of the spectroscopy pulse and of the data acquisition in Hertz.
+    ro_acquisition_delay
+        start of the data acquisition with respect to the start of the spectroscopy
+        pulse in seconds.
+    ro_integration_time
+        integration time of the data acquisition in seconds.
+    init_duration :
+        The relaxation time or dead time.
+    repetitions
+        The amount of times the Schedule will be repeated.
+    """
+    sched = Schedule("NCO pulsed spectroscopy", repetitions)
+    sched.add_resources(
+        [
+            ClockResource(name=spec_pulse_clock, freq=spec_pulse_frequencies.flat[0]),
+            ClockResource(name=ro_pulse_clock, freq=ro_pulse_frequency),
+        ]
+    )
+
+    sched.add(IdlePulse(duration=init_duration), label="buffer")
+
+    for acq_idx, spec_pulse_freq in enumerate(spec_pulse_frequencies):
+        sched.add(
+            SetClockFrequency(clock=spec_pulse_clock, clock_frequency=spec_pulse_freq),
+            label=f"set_clock_freq {acq_idx} ({spec_pulse_clock} {spec_pulse_freq:e} Hz)",
+        )
+
+        sched.add(
+            SquarePulse(
+                duration=spec_pulse_duration,
+                amp=spec_pulse_amp,
+                port=spec_pulse_port,
+                clock=spec_pulse_clock,
+            ),
+            label=f"spec_pulse {acq_idx}",
+        )
+
+        ro_pulse = sched.add(
+            SquarePulse(
+                duration=ro_pulse_duration,
+                amp=ro_pulse_amp,
+                port=ro_pulse_port,
+                clock=ro_pulse_clock,
+            ),
+            label=f"readout_pulse {acq_idx}",
+            rel_time=ro_pulse_delay,
+        )
+
+        sched.add(
+            SSBIntegrationComplex(
+                duration=ro_integration_time,
+                port=ro_pulse_port,
+                clock=ro_pulse_clock,
+                acq_index=0,
+                acq_channel=0,
+            ),
+            ref_op=ro_pulse,
+            ref_pt="start",
+            rel_time=ro_acquisition_delay,
+            label=f"acquisition {acq_idx}",
+        )
 
     return sched
 
