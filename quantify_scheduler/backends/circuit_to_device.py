636c3c0be52db3b9f6fb8ef7ff5bc7b3c523a06e
tobiasbonsen <tobias@orangeqs.com>
2023-03-01 14:00:32 +0100

    compile_circuit_to_device accepts either config or device_cfg

--- quantify_scheduler/backends/circuit_to_device.py
+++ quantify_scheduler/backends/circuit_to_device.py
@@ -21,6 +21,8 @@ from quantify_scheduler.schedules.schedule import Schedule
 
 def compile_circuit_to_device(
     schedule: Schedule,
+    config: Optional[CompilationConfig] = None,
+    # device_cfg for backwards compatibility:
     device_cfg: Optional[Union[DeviceCompilationConfig, dict]] = None,
 ) -> Schedule:
     """
@@ -32,6 +34,9 @@ def compile_circuit_to_device(
     ----------
     schedule
         The schedule to be compiled.
+    config
+        CompilationConfig used in the :class:`~QuantifyCompiler`, from which only
+        the :class:`~DeviceCompilationConfig` is used in this compilation step.
     device_cfg
         Device specific configuration, defines the compilation step from
         the quantum-circuit layer to the quantum-device layer description.
@@ -39,10 +44,15 @@ def compile_circuit_to_device(
         :class:`~DeviceCompilationConfig`.
 
     """
+    if config and device_cfg:
+        raise ValueError(
+            "compile_circuit_to_device with both a config=CompilationConfig and a device_cfg (deprecated). "
+            "Please make sure this function is called with either of the two (CompilationConfig recommended)."
+        )
     # In the graph-based compilation, CompilationNodes should accept the full
     # CompilationConfig as input (#405, !615, &1)
-    if isinstance(device_cfg, CompilationConfig):
-        device_cfg = device_cfg.device_compilation_config
+    if isinstance(config, CompilationConfig):
+        device_cfg = config.device_compilation_config
 
     if not isinstance(device_cfg, DeviceCompilationConfig):
         # this is a special case to be supported to enable compilation for schedules
