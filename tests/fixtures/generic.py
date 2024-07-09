39ed995417fddbbcb2d4b2138a77d95151b8d95c
Robert Sokolewicz <rsokolewicz@gmail.com>
2024-07-09 17:55:25 +0200

    close mpl figures after each test

--- tests/fixtures/generic.py
+++ tests/fixtures/generic.py
@@ -1,6 +1,13 @@
+import matplotlib.pyplot as plt
 import pytest
 
 
 @pytest.fixture
 def example_ip() -> str:
     return "192.168.1.100"
+
+
+@pytest.fixture(autouse=True)
+def close_figures():
+    yield
+    plt.close("all")
