358df7162891909be21f2f7798837f891571fd41
Gábor Oszkár Dénes <gdenes@qblox.com>
2024-09-12 09:12:20 +0000

    Net zero pulse UI changes

---
+++ quantify_scheduler/operations/pulse_compensation_library.py
@@ -0,0 +1,84 @@
+# Repository: https://gitlab.com/quantify-os/quantify-scheduler
+# Licensed according to the LICENCE file on the main branch
+"""Pulse compensation operations for use with the quantify_scheduler."""
+
+from __future__ import annotations
+
+from collections import namedtuple
+from typing import TYPE_CHECKING
+
+from quantify_scheduler.operations.operation import Operation
+
+if TYPE_CHECKING:
+    from quantify_scheduler.schedules.schedule import Schedule
+
+PortClock = namedtuple("PortClock", ["port", "clock"])
+
+
+class PulseCompensation(Operation):
+    """
+    Apply pulse compensation to an operation or schedule.
+
+    Inserts a pulse at the end of the operation or schedule set in ``body`` for each portclock.
+    The compensation pulses are calculated so that the integral of all pulses
+    (including the compensation pulses) are zero for each portclock.
+    Moreover, the compensating pulses are square pulses, and start just after the last
+    pulse on each portclock individually, and their maximum amplitude is the one
+    specified in the ``max_compensation_amp``. Their duration is divisible by ``duration_grid``.
+
+    Parameters
+    ----------
+    body
+        Operation to be pulse-compensated
+    max_compensation_amp
+        Dictionary for each portclock the maximum allowed amplitude for the compensation pulse.
+    time_grid
+        Grid time of the duration of the compensation pulse.
+    sampling_rate
+        Sampling rate for pulse integration calculation.
+    """
+
+    def __init__(
+        self,
+        body: Operation | Schedule,
+        max_compensation_amp: dict[PortClock, float],
+        time_grid: float,
+        sampling_rate: float,
+    ) -> None:
+        super().__init__(name="PulseCompensation")
+        self.data.update(
+            {
+                "pulse_compensation_info": {
+                    "body": body,
+                    "max_compensation_amp": max_compensation_amp,
+                    "time_grid": time_grid,
+                    "sampling_rate": sampling_rate,
+                },
+            }
+        )
+        self._update()
+
+    @property
+    def body(self) -> Operation | Schedule:
+        """Body of a pulse compensation."""
+        return self.data["pulse_compensation_info"]["body"]
+
+    @body.setter
+    def body(self, value: Operation | Schedule) -> None:
+        """Body of a pulse compensation."""
+        self.data["pulse_compensation_info"]["body"] = value
+
+    @property
+    def max_compensation_amp(self) -> dict[PortClock, float]:
+        """For each portclock the maximum allowed amplitude for the compensation pulse."""
+        return self.data["pulse_compensation_info"]["max_compensation_amp"]
+
+    @property
+    def time_grid(self) -> float:
+        """Grid time of the duration of the compensation pulse."""
+        return self.data["pulse_compensation_info"]["time_grid"]
+
+    @property
+    def sampling_rate(self) -> float:
+        """Sampling rate for pulse integration calculation."""
+        return self.data["pulse_compensation_info"]["sampling_rate"]
--- quantify_scheduler/pulse_compensation.py
+++ quantify_scheduler/pulse_compensation.py
@@ -6,7 +6,7 @@ from __future__ import annotations
 import math
 from copy import deepcopy
 from dataclasses import dataclass
-from typing import TYPE_CHECKING, Tuple
+from typing import TYPE_CHECKING, overload
 
 from quantify_scheduler.compilation import _determine_absolute_timing
 from quantify_scheduler.helpers.waveforms import area_pulse
@@ -14,14 +14,19 @@ from quantify_scheduler.operations.control_flow_library import (
     ControlFlowOperation,
     LoopOperation,
 )
+from quantify_scheduler.operations.pulse_compensation_library import (
+    PortClock,
+    PulseCompensation,
+)
+from quantify_scheduler.operations.pulse_library import SquarePulse
 from quantify_scheduler.resources import BasebandClockResource
 from quantify_scheduler.schedules.schedule import Schedule, ScheduleBase
 
 if TYPE_CHECKING:
+    from quantify_scheduler.backends.graph_compilation import (
+        CompilationConfig,
+    )
     from quantify_scheduler.operations.operation import Operation
-
-
-PortClock = Tuple[str, str]
 
 
 @dataclass
@@ -82,8 +87,7 @@ def _determine_sum_and_end_of_all_pulses(
     :
         The sum and end time of all the pulses as a `SumEnd`.
     """
-    # TODO: uncomment in SE-540.
-    # assert not isinstance(operation, PulseCompensation)
+    assert not isinstance(operation, PulseCompensation)
 
     if isinstance(operation, ScheduleBase):
         pulses_sum_end: dict[PortClock, SumEnd] = {}
@@ -115,25 +119,25 @@ def _determine_sum_and_end_of_all_pulses(
             return looped_pulses_sum_end
         else:
             raise ValueError(
-                f"Error calculating compensating pulse amplitude for '{operation}'. "
+                f"Error calculating compensation pulse amplitude for '{operation}'. "
                 f"This control flow operation type is not allowed "
-                f"in a pulse compensating structure. "
+                f"in a pulse compensation structure. "
             )
     elif operation.has_voltage_offset:
         raise ValueError(
-            f"Error calculating compensating pulse amplitude for '{operation}'. "
+            f"Error calculating compensation pulse amplitude for '{operation}'. "
             f"Voltage offset operation type is not allowed "
-            f"in a pulse compensating structure. "
+            f"in a pulse compensation structure. "
         )
     elif operation.valid_pulse:
         pulses_sum_end: dict[PortClock, SumEnd] = {}
         for pulse_info in operation["pulse_info"]:
             if pulse_info["clock"] != BasebandClockResource.IDENTITY:
                 raise ValueError(
-                    f"Error calculating compensating pulse amplitude for '{operation}'. "
+                    f"Error calculating compensation pulse amplitude for '{operation}'. "
                     f"Clock must be the baseband clock. "
                 )
-            port_clock: PortClock = (pulse_info["port"], pulse_info["clock"])
+            port_clock: PortClock = PortClock(pulse_info["port"], pulse_info["clock"])
             new_pulse_sum_end: dict[PortClock, SumEnd] = {
                 port_clock: SumEnd(
                     sum=area_pulse(pulse_info, sampling_rate),
@@ -155,64 +159,147 @@ class CompensationPulseParams:
     amp: float
 
 
-def determine_compensating_pulse(
-    schedule: Schedule,
-    compensating_max_amp: dict[PortClock, float],
+def _determine_compensation_pulse(
+    operation: Schedule | Operation,
+    max_compensation_amp: dict[PortClock, float],
     time_grid: float,
     sampling_rate: float,
 ) -> dict[PortClock, CompensationPulseParams]:
     """
-    Calculates the timing and the amplitude of a compensating pulse for each port clock.
+    Calculates the timing and the amplitude of a compensation pulse for each port clock.
     The `duration` and `amp` are calculated, with the requirements, that
-    if a compensating square pulse is inserted in this schedule at `start` with duration `duration`,
+    if a compensation square pulse is inserted in the schedule at `start` with duration `duration`,
     and amplitude `amp`, then
-    * the integral of all pulses in the schedule would equal to 0,
-    * the duration of the compensating pulse is divisible by `time_grid`,
-    * the compensating pulse is the last pulse in the schedule, and
-    * the compensating pulse starts just after the previous pulse.
+    * the integral of all pulses in the operation would equal to 0,
+    * the duration of the compensation pulse is divisible by `time_grid`,
+    * the compensation pulse is the last pulse in the operation, and
+    * the compensation pulse starts just after the previous pulse.
     The function assumes there is no operation which needs to be pulse compensated inside.
 
     Parameters
     ----------
-    schedule
-        The original schedule to compensate for.
-    compensating_max_amp
-        The maximum amplitude of the compensating pulse.
+    operation
+        The original operation or schedule to compensate for.
+    max_compensation_amp
+        The maximum amplitude of the compensation pulse.
     time_grid
-        Time grid the compensating pulse needs to be on.
+        Time grid the compensation pulse needs to be on.
     sampling_rate
         Sampling rate of the pulses.
 
     Returns
     -------
     :
-        The start, duration and amp of a compensating pulse
+        The start, duration and amp of a compensation pulse
         with the given requirements as a `CompensationPulseParams` for each port clock.
     """
     pulses_start_duration_amp: dict[PortClock, CompensationPulseParams] = {}
 
-    schedule_with_abs_times = _determine_absolute_timing(deepcopy(schedule), None)
+    operation_with_abs_times = (
+        operation
+        if not isinstance(operation, Schedule)
+        else _determine_absolute_timing(deepcopy(operation), None)
+    )
     pulses_sum_end: dict[PortClock, SumEnd] = _determine_sum_and_end_of_all_pulses(
-        schedule_with_abs_times, sampling_rate, 0
+        operation_with_abs_times, sampling_rate, 0
     )
 
     for port_clock, pulse_sum_end in pulses_sum_end.items():
-        sum_abs: float = abs(pulse_sum_end.sum)
+        if pulse_sum_end.sum != 0 and port_clock in max_compensation_amp:
+            sum_abs: float = abs(pulse_sum_end.sum)
 
-        if port_clock not in compensating_max_amp:
-            raise ValueError(
-                f"Error calculating compensating pulse amplitude for "
-                f"portclock '{port_clock[0]}-{port_clock[1]}'. "
-                f"No maximum amplitude for portclock found, make sure it's set."
+            duration: float = (
+                math.ceil(sum_abs / time_grid / max_compensation_amp[port_clock])
+                * time_grid
+            )
+            amp: float = -pulse_sum_end.sum / duration
+            pulses_start_duration_amp[port_clock] = CompensationPulseParams(
+                start=pulse_sum_end.end, duration=duration, amp=amp
             )
 
-        duration: float = (
-            math.ceil(sum_abs / time_grid / compensating_max_amp[port_clock])
-            * time_grid
+    return pulses_start_duration_amp
+
+
+@overload
+def process_compensation_pulses(
+    schedule: Schedule,
+    config: CompilationConfig | None = None,
+) -> Schedule: ...
+@overload
+def process_compensation_pulses(
+    schedule: Operation,
+    config: CompilationConfig | None = None,
+) -> Schedule | Operation: ...
+def process_compensation_pulses(
+    schedule: Schedule | Operation,
+    config: CompilationConfig | None = None,
+) -> Schedule | Operation:
+    """
+    Replaces ``PulseCompensation`` with a subschedule with an additional compensation pulse.
+
+    Parameters
+    ----------
+    schedule
+        The schedule which contains potential ``PulseCompensation`` in it.
+    config
+        Compilation config for
+        :class:`~quantify_scheduler.backends.graph_compilation.QuantifyCompiler`.
+
+
+    Returns
+    -------
+    :
+        The start, duration and amp of a compensation pulse
+        with the given requirements as a `CompensationPulseParams` for each port clock.
+    """
+    operation: Operation | Schedule = schedule
+    if isinstance(operation, ScheduleBase):
+        for inner_op_key in operation.operations:
+            # Here, we modify the subschedule, and every time the same
+            # subschedule is reused in other parts of the whole schedule,
+            # will also be modified. We assume those are compiled the same way every time.
+            operation.operations[inner_op_key] = process_compensation_pulses(
+                operation.operations[inner_op_key],
+                config,
+            )
+        return operation
+    elif isinstance(operation, ControlFlowOperation):
+        operation.body = process_compensation_pulses(
+            operation.body,
+            config,
         )
-        amp: float = -pulse_sum_end.sum / duration
-        pulses_start_duration_amp[port_clock] = CompensationPulseParams(
-            start=pulse_sum_end.end, duration=duration, amp=amp
+        return operation
+    elif isinstance(operation, PulseCompensation):
+        # Inner pulse compensated blocks need to be resolved first.
+        resolved_body = process_compensation_pulses(operation.body)
+        all_compensation_pulse_params: dict[PortClock, CompensationPulseParams] = (
+            _determine_compensation_pulse(
+                resolved_body,
+                operation.max_compensation_amp,
+                operation.time_grid,
+                operation.sampling_rate,
+            )
         )
 
-    return pulses_start_duration_amp
+        pulse_compensated_schedule = Schedule("pulse_compensated_schedule")
+        first_op_schedulable = pulse_compensated_schedule.add(resolved_body)
+
+        for (
+            port_clock,
+            compensation_pulse_params,
+        ) in all_compensation_pulse_params.items():
+            pulse_compensated_schedule.add(
+                operation=SquarePulse(
+                    amp=compensation_pulse_params.amp,
+                    duration=compensation_pulse_params.duration,
+                    port=port_clock.port,
+                    clock=port_clock.clock,
+                ),
+                rel_time=compensation_pulse_params.start,
+                ref_op=first_op_schedulable,
+                ref_pt="start",
+                ref_pt_new="start",
+            )
+
+        return pulse_compensated_schedule
+    return schedule
