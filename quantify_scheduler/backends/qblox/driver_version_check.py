ffe4fc796980ff2e6a6d0df3c649568b809f9114
Robert Sokolewicz <rsokolewicz@gmail.com>
2025-11-04 23:36:24 +0100

    fix: update qblox driver check

--- quantify_scheduler/backends/qblox/driver_version_check.py
+++ quantify_scheduler/backends/qblox/driver_version_check.py
@@ -13,7 +13,7 @@ try:
 except ImportError:
     driver_version = None  # Prior to v0.3.2 __version__ was not there
 
-SUPPORTED_DRIVER_VERSIONS: tuple[str, ...] = ("0.17",)
+SUPPORTED_DRIVER_VERSIONS: tuple[str, ...] = ("0.17", "1")
 """Tuple containing all the version supported by this version of the backend."""
 raise_on_version_mismatch: bool = True
 """Can be set to false to override version check."""
